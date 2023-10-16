165fc15b1bec3a108b43a771854a34f1a0fb548c
olivierbabasse <34944436+olivierbabasse@users.noreply.github.com>
2023-10-16 21:30:32 +0200

    solved lc0020

---
+++ src/solutions/lc0020_valid_parentheses.rs
@@ -0,0 +1,33 @@
+//! <https://leetcode.com/problems/valid-parentheses/>
+
+struct Solution {}
+
+impl Solution {
+    pub fn is_valid(s: String) -> bool {
+        let mut stack = Vec::new();
+        for c in s.chars() {
+            match c {
+                '(' | '[' | '{' => stack.push(c),
+                _ => match stack.pop() {
+                    Some('(') if c == ')' => {}
+                    Some('[') if c == ']' => {}
+                    Some('{') if c == '}' => {}
+                    _ => return false,
+                },
+            }
+        }
+        stack.is_empty()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+
+    #[test]
+    fn test_cases() {
+        assert!(Solution::is_valid("()".into()));
+        assert!(Solution::is_valid("()[]{}".into()));
+        assert!(!Solution::is_valid("(]".into()));
+    }
+}
