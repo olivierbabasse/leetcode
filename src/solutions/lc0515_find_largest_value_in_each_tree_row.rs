61e88dec521a23eb35b18ee00490d88383235380
olivierbabasse <34944436+olivierbabasse@users.noreply.github.com>
2023-10-24 12:50:54 +0200

    solved lc0515

---
+++ src/solutions/lc0515_find_largest_value_in_each_tree_row.rs
@@ -0,0 +1,60 @@
+//! <https://leetcode.com/problems/find-largest-value-in-each-tree-row/>
+
+use std::cell::RefCell;
+use std::rc::Rc;
+
+type TreeNode = crate::utils::tree::TreeNode<i32>;
+
+struct Solution {}
+
+/// time-complexity : O(n)
+/// space-complexity : O(n)
+impl Solution {
+    fn dfs(root: &Option<Rc<RefCell<TreeNode>>>, depth: usize, maximums: &mut Vec<i32>) {
+        if root.is_none() {
+            return;
+        }
+
+        let root = root.as_ref().unwrap().borrow();
+
+        if maximums.len() <= depth {
+            maximums.push(root.val);
+        } else {
+            maximums[depth] = maximums[depth].max(root.val);
+        }
+
+        Self::dfs(&root.left, depth + 1, maximums);
+        Self::dfs(&root.right, depth + 1, maximums);
+    }
+
+    pub fn largest_values(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<i32> {
+        let mut maximums = Vec::new();
+        Self::dfs(&root, 0, &mut maximums);
+        maximums
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_cases() {
+        assert_eq!(
+            Solution::largest_values(Some(Rc::new(RefCell::new(TreeNode::from_vec_opt(&[
+                Some(1),
+                Some(3),
+                Some(2),
+                Some(5),
+                Some(3),
+                None,
+                Some(9)
+            ]))))),
+            vec![1, 3, 9]
+        );
+        assert_eq!(
+            Solution::largest_values(Some(Rc::new(RefCell::new(TreeNode::from_vec(&[1, 2, 3]))))),
+            vec![1, 3]
+        );
+    }
+}
--- src/solutions/mod.rs
+++ src/solutions/mod.rs
@@ -17,6 +17,7 @@ mod lc0342_power_of_four;
 mod lc0343_integer_break;
 mod lc0383_ransom_note;
 mod lc0412_fizz_buzz;
+mod lc0515_find_largest_value_in_each_tree_row;
 mod lc0557_reverse_words_in_a_string_iii;
 mod lc0706_design_hashmap;
 mod lc0746_min_cost_climbing_stairs;
